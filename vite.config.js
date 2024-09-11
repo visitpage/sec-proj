ab94baa246c27eee67da6041461c3a32ac34615b
visitpage <104766582+visitpage@users.noreply.github.com>
2024-09-12 02:07:34 +0800

    solve require console error

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+// import commonjs from 'vite-plugin-commonjs'
+// import commonjs from '@rollup/plugin-commonjs'
 
 // https://vitejs.dev/config/
+import { fileURLToPath, URL } from 'node:url'
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  build: {
+    commonjsOptions: { transformMixedEsModules: true } // Change
+  }
 })
